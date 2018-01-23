8910239b0d025c8ae34a19087037910002f71eac
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-23 23:01:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ function deepestChild(){
     if (criteriaFn(current)) {
       return current
     }
-    
+     if (Array.isArray(current)) {
+      for (let i = 0; i < current.length; i++) {
+        next.push(current[i])
+      }
+    }
   
   
 }
