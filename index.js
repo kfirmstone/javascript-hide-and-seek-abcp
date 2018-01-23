f0944f9c316b7dd1e82836c1200e402175162f7d
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-23 22:56:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ function increaseRankBy(n) {
 }
 
 function deepestChild(){
-  let current = document.getElementById('')
+  let current = document.getElementById('grand-node')
+  let next =
+  
   
 }
 
