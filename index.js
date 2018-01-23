05a8160a2e9c5892d17f1dfd23a261ad8269a78b
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-23 22:51:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,9 @@ function nestedTarget(){
   return document.getElementById('nested').querySelector(".target")
 }
 function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list')
-
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children
-
-    for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n
-    }
+  var lis=document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for(let i=0;i<lis.length;i++) {
+    lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
   }
 }
-
 
